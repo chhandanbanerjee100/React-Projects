224d0fe1a1022f04cf3ec9baca1f6c3711fa6442
chandanbanerjee10 <chandanbanerjee514@gmail.com>
2023-06-01 20:52:52 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import './App.css';
 
-import Sidebar  from './components/third_project/atoms/Sidebar';
-import Route from './components/third_project/atoms/Route';
-import AccordionPage from './components/third_project/pages/AccordionPage';
-import DropdownPage from './components/third_project/pages/DropdownPage';
-import ButtonPage from './components/third_project/pages/ButtonPage';
-import ModalPage from './components/third_project/pages/ModalPage';
-import TablePage from './components/third_project/pages/TablePage';
+import Index from './components/third_project/Index';
 
 function App() {
   return (
-    <div className='text-2xl container mx-auto grid grid-cols-6 gap-4 mt-4'>
+    <div>
       {/* <Api_Project /> */}
       {/* <BookProject /> */}
-      {/* <Index /> */}
-      <Sidebar />
+      <Index /> {/* 3rd Project */}
 
-      <div className='col-span-5'>
-        <Route path="/accordion">
-          <AccordionPage />
-        </Route>
-        <Route path="/dropdown">
-          <DropdownPage />
-        </Route>
-        <Route path="/buttons">
-          <ButtonPage />
-        </Route>
-        <Route path="/modal">
-          <ModalPage />
-        </Route>
-        <Route path="/table">
-          <TablePage />
-        </Route>
-      </div>
     </div>
   );
 }
--- src/components/third_project/Index.js
+++ src/components/third_project/Index.js
@@ -1,12 +1,39 @@
 // import DropdownPage from "./pages/DropdownPage"
 // import ButtonPage from "./pages/ButtonPage";
 // import AccordionPage from "./pages/AccordionPage";
+import Sidebar  from './atoms/Sidebar';
+import Route from './atoms/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
+import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
 function Index() {
 
   return(
     // <DropdownPage /><ello></ello>
     // <AccordionPage />
-    <div></div>
+    <div className='text-2xl container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <Sidebar />
+
+      <div className='col-span-5'>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
+        </Route>
+      </div>
+    </div>
   )
 }
 
